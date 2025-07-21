a379e83de5fd8f64e2569a0ec3b73744fe19b76d
Noureddine Ben sadok <noureddine.bensadok1@gmail.com>
2025-07-21 16:13:30 +0200

    tp2 compte bancaire + points bonus

--- hello_rust/src/compteBancaire.rs
+++ hello_rust/src/compteBancaire.rs
@@ -6,7 +6,7 @@ struct CompteBancaire {
 impl CompteBancaire{
 
     fn afficher(&self){
-        println!("Compte de {} : {} €:", self.nom, self.solde);
+        println!("Compte de {} : {} €", self.nom, self.solde);
     }
 
     fn deposer(&mut self, montant:f64){
@@ -43,18 +43,44 @@ impl CompteBancaire{
 
 
 fn main() {
-    let mut compte = CompteBancaire {
+    let mut compte_nb = CompteBancaire {
         nom: "Noureddine".to_string(),
         solde: 1000.0,
     };
 
-    compte.afficher();
-    
-    compte.deposer(500.0);
-    compte.afficher();
-    
-    compte.retirer(200.0);
-    compte.afficher();
+    compte_nb.afficher();
 
-    compte.fermer();
+    compte_nb.deposer(500.0);
+    compte_nb.afficher();
+
+    compte_nb.retirer(200.0);
+    compte_nb.renommer("Noureddine B".to_string());
+    compte_nb.afficher();
+
+    compte_nb.fermer();
+    
+    // créer un Vec<CompteBancaire> pour gérer plusieurs comptes ( en utilisant .iter(), .enumerate() )
+    let mut comptes: Vec<CompteBancaire> = Vec::new();
+    comptes.push(CompteBancaire {
+        nom: "Alice".to_string(),
+        solde: 1500.0,
+    });
+    comptes.push(CompteBancaire {
+        nom: "Bob".to_string(),
+        solde: 2000.0,
+    });
+    comptes.push(CompteBancaire {
+        nom: "Charlie".to_string(),
+        solde: 3000.0,
+    });
+    for (_index, compte) in comptes.iter().enumerate() {
+        compte.afficher();
+    }
+
+    comptes[0].deposer(100.0);
+    comptes[1].retirer(500.0);
+    comptes[2].renommer("Charlie Brown".to_string());
+    for (_index, compte) in comptes.iter().enumerate() {
+        compte.afficher();
+    }
 }
